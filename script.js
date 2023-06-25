c0ac0e50fd0980cc9a763e795fb731b5b1ef7204
Trinh Van Son <sontrinh.tp@gmail.com>
2023-06-25 23:24:24 +0700

    add file

---
+++ script.js
@@ -0,0 +1,14 @@
+function isFirstLetterUpperCase(str) {
+    const REGEX = /^[A-Z]/;
+    if (REGEX.test(str)) {
+        console.log("String's first character is uppercase");
+    } else {
+        console.log("String's first character is not uppercase");
+    }
+}
+
+isFirstLetterUpperCase('Abcd');
+isFirstLetterUpperCase('abcd');
+isFirstLetterUpperCase('Nguyen van Nam');
+isFirstLetterUpperCase('NGUYEN VAN NAM');
+isFirstLetterUpperCase('nguyen van Nam');
